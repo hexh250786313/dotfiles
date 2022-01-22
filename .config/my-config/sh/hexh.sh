83e7843adde644a885b563a49c121db296989d8b
hexh <250786313@qq.com>
2022-01-22 21:59:45 +0800

    perf: update

--- .config/my-config/sh/hexh.sh
+++ .config/my-config/sh/hexh.sh
@@ -6,6 +6,8 @@ tmux has-session -t $SESSIONNAME_0
 if [ "$?" != 0 ] 
  then
     tmux new-session -s $SESSIONNAME_0 -n main -d
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux set-option repeat-time 0" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "clear" C-m
 fi
 
 if [ -n "$TMUX" ]
--- .config/my-config/sh/work.sh
+++ .config/my-config/sh/work.sh
@@ -5,23 +5,30 @@ tmux has-session -t $SESSIONNAME_0
 
 if [ "$?" != 0 ] 
  then
-    tmux new-session -s $SESSIONNAME_0 -n main -d
-    tmux send-keys -t $SESSIONNAME_0 "tmux new-window -n code" C-m
-    # sleep 2 && tmux send-keys -t $SESSIONNAME_0:3 "all_proxy='' http_proxy='' https_proxy='' musicbox" C-m
-    tmux send-keys -t $SESSIONNAME_0 "tmux new-window -n server" C-m
-    tmux send-keys -t $SESSIONNAME_0 "tmux new-window -n 10.10.5.28" C-m
-    tmux send-keys -t $SESSIONNAME_0 "tmux new-window -n music" C-m
-    sleep 2; tmux send-keys -t $SESSIONNAME_0:3 "wd workspace" C-m
-    tmux send-keys -t $SESSIONNAME_0:3 "cd MOBILE" C-m
-    # tmux send-keys -t $SESSIONNAME_0:3 "gl && yarn && yarn start" C-m
-    tmux split-window -h -t $SESSIONNAME_0:3
-    tmux send-keys -t $SESSIONNAME_0:3 "wd workspace" C-m
-    tmux send-keys -t $SESSIONNAME_0:3 "cd MANAGEMENT" C-m
-    # tmux send-keys -t $SESSIONNAME_0:3 "gl && yarn && yarn start" C-m
-    # tmux send-keys -t $SESSIONNAME_0:4 "cd ~/.config/openvpn/howard/ && echo 'lllk' | sudo -S openvpn howard.ovpn" C-m
-    tmux split-window -h -t $SESSIONNAME_0:4
-    # tmux send-keys -t $SESSIONNAME_0:4 "sleep 10 && ssh node@10.10.5.28" C-m
-    # tmux send-keys -t $SESSIONNAME_0:4 "frontend-build" C-m
+    # 创建 session 和 window
+    tmux new-session -s $SESSIONNAME_0:1 -n main -d
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux set-option repeat-time 50" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n code" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n server" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n 10.10.5.28" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n music" C-m
+
+    # 发送指令
+    # Sample
+    # tmux send-keys -t $SESSIONNAME_0:1 "" C-m
+
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'wd workspace && cd MOBILE' C-m" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'gl && yarn && yarn start' C-m" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux split-window -h -t $SESSIONNAME_0:3" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'wd workspace && cd MANAGEMENT' C-m" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'gl && yarn && yarn start' C-m" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:4 'howard' C-m" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux split-window -h -t $SESSIONNAME_0:4" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:4 'sleep 10 && ssh node@10.10.5.28 && frontend-build' C-m" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:5 'all_proxy="" http_proxy="" https_proxy="" musicbox' C-m" C-m
+
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux set-option repeat-time 0" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "clear" C-m
 fi
 
 if [ -n "$TMUX" ]
