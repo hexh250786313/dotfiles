b81d01d7088ff10aafd56662c84cd75e41501e17
hexh <250786313@qq.com>
2022-03-06 22:02:39 +0800

    perf(nvim): update

--- .config/my-config/sh/work.sh
+++ .config/my-config/sh/work.sh
@@ -9,21 +9,21 @@ if [ "$?" != 0 ]
     tmux new-session -s $SESSIONNAME_0 -n main -d
     # tmux send-keys -t $SESSIONNAME_0:1 "tmux set-option repeat-time 50" C-m
     tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n code" C-m
-    tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n server" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n server" C-m
     tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n 10.10.5.28" C-m
-    tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n music" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux new-window -n music" C-m
 
     # 发送指令
     # Sample
     # tmux send-keys -t $SESSIONNAME_0:1 "" C-m
 
-    tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'wd workspace && cd MOBILE' C-m" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'wd workspace && cd MOBILE' C-m" C-m
     # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'gl && yarn && yarn start' C-m" C-m
-    tmux send-keys -t $SESSIONNAME_0:1 "tmux split-window -h -t $SESSIONNAME_0:3" C-m
-    tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'wd workspace && cd MANAGEMENT' C-m" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux split-window -h -t $SESSIONNAME_0:3" C-m
+    # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'wd workspace && cd MANAGEMENT' C-m" C-m
     # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:3 'gl && yarn && yarn start' C-m" C-m
     # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:4 'howard' C-m" C-m
-    tmux send-keys -t $SESSIONNAME_0:1 "tmux split-window -h -t $SESSIONNAME_0:4" C-m
+    tmux send-keys -t $SESSIONNAME_0:1 "tmux split-window -h -t $SESSIONNAME_0:3" C-m
     # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:4 'sleep 10 && ssh node@10.10.5.28 && frontend-build' C-m" C-m
     # tmux send-keys -t $SESSIONNAME_0:1 "tmux send-keys -t $SESSIONNAME_0:5 'all_proxy="" http_proxy="" https_proxy="" musicbox' C-m" C-m
 
