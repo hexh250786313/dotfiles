c06a21aa62f3fd12590d3a1e897e5c95707a54fd
hexh <250786313@qq.com>
2022-04-28 14:07:10 +0800

    perf: nvim focus

--- .config/nvim/lua/plugin/focus.lua
+++ .config/nvim/lua/plugin/focus.lua
@@ -3,3 +3,8 @@ local api = vim.api
 require("focus").setup({enable = true})
 api.nvim_set_keymap("n", "<space>wf", ":FocusToggle<CR>", {silent = true})
 api.nvim_create_autocmd({"VimEnter"}, {pattern = "*", command = "FocusDisable"})
+
+vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = "wincmd H"})
+vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = "vertical resize 40"})
+vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = "set winfixwidth"})
+vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = "set winfixheight"})
--- .config/nvim/lua/user/autocmd.lua
+++ .config/nvim/lua/user/autocmd.lua
@@ -1,7 +1,3 @@
 vim.cmd([[
 ]])
 
-vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = "wincmd H"})
-vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = ":vertical resize 40"})
-vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = ":set winfixwidth"})
-vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = ":set winfixheight"})
