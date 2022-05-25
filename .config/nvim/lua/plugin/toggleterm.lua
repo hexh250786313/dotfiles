57f1be1578ca34e414c5b7bf997cfe8f02897be1
hexh <250786313@qq.com>
2022-05-25 21:41:49 +0800

    perf: update

--- .config/nvim/lua/plugin/toggleterm.lua
+++ .config/nvim/lua/plugin/toggleterm.lua
@@ -19,6 +19,21 @@ end
 
 vim.api.nvim_set_keymap("n", "<space>gl", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
-vim.cmd([[
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, "t", "<esc><esc>", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<space>'", [[<C-\><C-n>:q<cr>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd(
+  [[
+autocmd! TermOpen term://* lua set_terminal_keymaps()
+
 nnoremap <silent> <Space>' <cmd>ToggleTerm direction=float<cr>
-]])
+]]
+)
