950abdd73d10719830f348346883d8a6e6a8b6b0
hexh <250786313@qq.com>
2022-06-17 15:53:54 +0800

    perf: update

--- .config/nvim/lua/plugin/nvim-window.lua
+++ .config/nvim/lua/plugin/nvim-window.lua
@@ -1,3 +1,9 @@
-require("nvim-window").setup({})
+require("nvim-window").setup(
+  {
+    normal_hl = "Visual",
+    hint_hl = "Bold",
+    border = "none"
+  }
+)
 
 vim.api.nvim_set_keymap("n", "<space>ww", ":lua require('nvim-window').pick()<cr>", {silent = true})
