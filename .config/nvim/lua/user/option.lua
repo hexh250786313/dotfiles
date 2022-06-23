cccea1d84cc673ba6770280c808f41978d076455
hexh <250786313@qq.com>
2022-06-23 20:20:23 +0800

    wip

--- .config/nvim/lua/user/option.lua
+++ .config/nvim/lua/user/option.lua
@@ -5,7 +5,7 @@ set noincsearch " / 搜索时, 在按下 <cr> 前不会乱跳
 
 local optionList = {
   mouse = "a", -- 使用鼠标
-  number = true, -- 显示行号
+  -- number = true, -- 显示行号, 如果开了, 则出现丑的要命的右 padding
   relativenumber = true, -- 使用相对行号
   title = true, -- 任务栏显示标题
   hlsearch = true, -- 光标搜索
@@ -17,7 +17,7 @@ local optionList = {
   foldlevel = 99, -- 折叠级别
   wrap = false, -- 是否折行
   swapfile = false, -- 是否使用交换文件
-  signcolumn = "yes:1", -- icon 展示在哪 "number" 为展示在行号上
+  signcolumn = "yes:1", -- icon 展示在哪 "number" 为展示在行号上, 如果完全没有 1 行的话, 就是没有 padding, 有点挤, 不好看
   lazyredraw = true, -- 执行脚本时不刷新
   wildignorecase = true, -- 弹窗忽略大小写
   -- foldmethod = "expr", -- 折叠方式
