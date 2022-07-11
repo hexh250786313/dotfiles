c0c4c142183725c02b2598b05f36be074983abd5
hexh <250786313@qq.com>
2022-07-11 18:16:50 +0800

    wip

--- .config/nvim/lua/plugin/neoscroll.lua
+++ .config/nvim/lua/plugin/neoscroll.lua
@@ -1,3 +1,10 @@
+vim.keymap.set('n', '<ScrollWheelUp>', '<C-y>')
+vim.keymap.set('n', '<ScrollWheelDown>', '<C-e>')
+-- vim.keymap.set('i', '<ScrollWheelUp>', '<C-y>')
+-- vim.keymap.set('i', '<ScrollWheelDown>', '<C-e>')
+-- vim.keymap.set('v', '<ScrollWheelUp>', '<C-y>')
+-- vim.keymap.set('v', '<ScrollWheelDown>', '<C-e>')
+
 require("neoscroll").setup(
   {
     easing_function = "nil",
