bce1eaaf0309b46a5e0240206b0316228d73a8ce
hexh <250786313@qq.com>
2022-07-11 17:52:36 +0800

    wip

--- .config/i3/scripts/cycle-windows.sh
+++ .config/i3/scripts/cycle-windows.sh
@@ -1,24 +1,19 @@
-id=( $(wmctrl -l | awk "/$rx/"'{print $1}') )
+id=($(wmctrl -l | awk "/$rx/"'{print $1}'))
 # id=( $(xprop -root | grep _NET_CLIENT_LIST_STACKING\(WINDOW | cut -d\  -f5- | awk '{for(i=1;i<=NF;i++){gsub(",", "", $i); printf("%s\n"), $i};}' | tac) )
 current=$(xdotool getactivewindow)
 
-# test=$(( ${#id[@]} ))
-
-# echo $test
-
-for (( i=1; i<$(( ${#id[@]} + 1 )); i++ )) ;do
-    # echo $i
-    # echo ${id[$i]}
-    if  [[ $current -eq ${id[$i]} ]]; then
-      # echo ${id[$i]}
-      max=$(( ${#id[*]} ))
+if [[ $current ]]; then
+  for ((i = 1; i < $((${#id[@]} + 1)); i++)); do
+    if [[ $current -eq ${id[$i]} ]]; then
+      max=$((${#id[*]}))
       if [[ $i -lt $max ]]; then
-        echo "1"
-        wmctrl -i -a ${id[i+1]}
+        wmctrl -i -a ${id[i + 1]}
       else
-        echo "2"
         wmctrl -i -a ${id[1]}
       fi
       # sleep 3
     fi
-done
+  done
+else
+  wmctrl -i -a ${id[1]}
+fi
