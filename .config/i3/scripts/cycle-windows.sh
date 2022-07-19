80a236502dcc0141b6195553e6cb69f41e1e8297
hexh <250786313@qq.com>
2022-07-19 13:35:18 +0800

    wip

--- .config/i3/scripts/cycle-windows.sh
+++ .config/i3/scripts/cycle-windows.sh
@@ -1,17 +1,34 @@
-id=($(wmctrl -l | awk "/$rx/"'{print $1}'))
-# id=( $(xprop -root | grep _NET_CLIENT_LIST_STACKING\(WINDOW | cut -d\  -f5- | awk '{for(i=1;i<=NF;i++){gsub(",", "", $i); printf("%s\n"), $i};}' | tac) )
+local allId
+local id=()
+allId=($(wmctrl -l | awk "/$rx/"'{print $1}'))
+# allId=( $(xprop -root | grep _NET_CLIENT_LIST_STACKING\(WINDOW | cut -d\  -f5- | awk '{for(i=1;i<=NF;i++){gsub(",", "", $i); printf("%s\n"), $i};}' | tac) )
 current=$(xdotool getactivewindow)
+
+for ((i = 1; i < $((${#allId[@]} + 1)); i++)); do
+  max=$((${#allId[*]}))
+  class=($(xprop -id ${allId[$i]} | grep "WM_CLASS(STRING)" | awk "/$rx/"'{print $4}'))
+  if [[ $class != '"Vmware"' ]]; then
+    # echo "1"
+    id+=(${allId[$i]})
+  fi
+done
+
+max=$((${#id[*]}))
+# echo $max
+# echo $id
 
 if [[ $current ]]; then
   for ((i = 1; i < $((${#id[@]} + 1)); i++)); do
+    # echo ${id[$i]}
     if [[ $current -eq ${id[$i]} ]]; then
-      max=$((${#id[*]}))
+      local next
       if [[ $i -lt $max ]]; then
-        wmctrl -i -a ${id[i + 1]}
+        next=${id[$(($i + 1))]}
       else
-        wmctrl -i -a ${id[1]}
+        next=${id[1]}
       fi
-      # sleep 3
+      # echo $next
+      wmctrl -i -a ${next}
     fi
   done
 else
