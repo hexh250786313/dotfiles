b0f6f4614c00608889a6fb47b023205018fad885
hexh <250786313@qq.com>
2022-07-29 10:56:51 +0800

    wip

--- .config/nvim/lua/user/autocmd.lua
+++ .config/nvim/lua/user/autocmd.lua
@@ -1,3 +1,11 @@
 vim.cmd([[
+function OpenQF ()
+  :wincmd H
+  :vertical resize 40
+  :set winfixwidth
+  :set winfixheight
+endfunction
 ]])
+
+vim.api.nvim_create_autocmd({"FileType, BufferEnter"}, {pattern = "qf", command = "silent call OpenQF()"})
 
