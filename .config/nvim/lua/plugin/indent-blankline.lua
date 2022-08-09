953ddccd8ce785c0fe2ebea59797158d14e1c14b
hexh <250786313@qq.com>
2022-08-09 19:54:40 +0800

    wip

--- .config/nvim/lua/plugin/indent-blankline.lua
+++ .config/nvim/lua/plugin/indent-blankline.lua
@@ -4,6 +4,7 @@ require("indent_blankline").setup {
   space_char_blankline = " ",
   show_current_context = true,
   show_current_context_start = true,
+  show_first_indent_level = false, -- 是否展示第一级
   filetype_exclude = {
     "help",
     "dashboard",
