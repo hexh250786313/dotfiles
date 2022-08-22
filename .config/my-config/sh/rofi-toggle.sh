b942b670315ec567e519fe7e037b8203c2e985e6
hexh <250786313@qq.com>
2022-08-22 09:01:11 +0800

    wip

--- .config/my-config/sh/rofi-toggle.sh
+++ .config/my-config/sh/rofi-toggle.sh
@@ -6,3 +6,12 @@ else
   # notify-send "hello" -t 5
   rofi -normal-window -combi-modi window,drun,ssh,run,combi -show combi -run-shell-command '{terminal} -e zsh -ic "{cmd} && read"'
 fi
+
+# rofi \
+    # -show window  \
+    # -kb-cancel "Alt+Escape,Escape" \
+    # -kb-accept-entry "!Alt-Tab,Return"\
+    # -kb-row-down "Alt+Tab,Alt+Down" \
+    # -kb-row-up "Alt+Shift+Tab,Alt+Up"&
+# xdotool keyup Tab
+# xdotool keydown Tab
