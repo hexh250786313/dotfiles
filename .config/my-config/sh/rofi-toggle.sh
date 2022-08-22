389acd2ba46decc404b619bff20f17d0c9b5098e
hexh <250786313@qq.com>
2022-08-22 14:45:23 +0800

    wip

--- .config/my-config/sh/rofi-toggle.sh
+++ .config/my-config/sh/rofi-toggle.sh
@@ -5,5 +5,5 @@ if pgrep -x rofi; then
 else
   # notify-send "hello" -t 5
   # rofi -combi-modi drun,ssh,run,combi -show combi -run-shell-command '{terminal} -e zsh -ic "{cmd} && read"'
-  rofi -normal-window -combi-modi window,drun,ssh,run,combi -show combi -run-shell-command '{terminal} -e zsh -ic "{cmd} && read"' # normal-window 是为了检测有无启动 rofi 以便 toggle, 不是 normal 检测不了
+  rofi -normal-window -combi-modi drun,ssh,run,combi -show combi -run-shell-command '{terminal} -e zsh -ic "{cmd} && read"' # normal-window 是为了检测有无启动 rofi 以便 toggle, 不是 normal 检测不了
 fi
