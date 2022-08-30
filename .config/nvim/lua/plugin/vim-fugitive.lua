bf6fb095f8b4b2844860f4bfcf41c179d9459050
hexh <250786313@qq.com>
2022-08-31 01:42:55 +0800

    wip

--- .config/nvim/lua/plugin/vim-fugitive.lua
+++ .config/nvim/lua/plugin/vim-fugitive.lua
@@ -1,7 +1,17 @@
 vim.g.fugitive_pty = 0
 
-vim.cmd([[
-nnoremap <silent> <Space>gc :Git commit<CR>
-nnoremap <silent> <Space>gg :Git<CR>
-]])
+vim.cmd(
+  [[
+function! ToggleGStatus()
+  if buflisted(bufname('.git//'))
+    bd .git//
+  else
+    Git
+  endif
+endfunction
 
+nnoremap <silent> <Space>gc :Git commit<CR>
+" nnoremap <silent> <Space>gg :Git<CR>
+nnoremap <silent> <Space>gg :<C-u>call ToggleGStatus()<cr>
+]]
+)
