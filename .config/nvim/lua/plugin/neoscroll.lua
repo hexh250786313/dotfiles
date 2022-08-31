4f7d3353981f97036315e78665936f95e6f1ed4a
hexh <250786313@qq.com>
2022-08-31 14:55:23 +0800

    wip

--- .config/nvim/lua/plugin/neoscroll.lua
+++ .config/nvim/lua/plugin/neoscroll.lua
@@ -21,9 +21,9 @@ local t = {}
 -- -- t["<Down>"] = {"scroll", {"3", "false", "50", [['sine']]}}
 -- t["<Up>"] = {"scroll", {"-3", "true", "50", [['sine']]}}
 -- t["<Down>"] = {"scroll", {"3", "true", "50", [['sine']]}}
--- t["zt"] = {"zt", {"200"}}
--- t["zz"] = {"zz", {"200"}}
--- t["zb"] = {"zb", {"200"}}
+t["zt"] = {"zt", {"200"}}
+t["zz"] = {"zz", {"200"}}
+t["zb"] = {"zb", {"200"}}
 
 t["<S-Up>"] = {"scroll", {"-vim.wo.scroll", "true", "100", [['circular']]}}
 t["<S-Down>"] = {"scroll", {"vim.wo.scroll", "true", "100", [['circular']]}}
@@ -31,8 +31,5 @@ t["<S-Down>"] = {"scroll", {"vim.wo.scroll", "true", "100", [['circular']]}}
 -- t["<Down>"] = {"scroll", {"3", "false", "50", [['quadratic']]}}
 t["<Up>"] = {"scroll", {"-3", "true", "50", [['quadratic']]}}
 t["<Down>"] = {"scroll", {"3", "true", "50", [['quadratic']]}}
-t["zt"] = {"zt", {"200", [['circular']]}}
-t["zz"] = {"zz", {"200", [['circular']]}}
-t["zb"] = {"zb", {"200", [['circular']]}}
 
 require("neoscroll.config").set_mappings(t)
