6479c549809fc64174d6471d1b1c9ea459eebdbe
hexh <250786313@qq.com>
2022-09-22 01:15:29 +0800

    wip

--- .config/nvim/lua/plugin/fzf.lua
+++ .config/nvim/lua/plugin/fzf.lua
@@ -1,1 +1,20 @@
 vim.keymap.set("n", "<space>sf", ":FZF<cr>", {silent = true})
+
+vim.cmd(
+  [[
+let g:fzf_colors =
+\ { 'fg':      ['fg', 'Comment'],
+  \ 'bg':      ['bg', 'StatusLine'],
+  \ 'hl':      ['fg', 'DiagnosticInfo'],
+  \ 'fg+':     ['fg', 'CursorLine', 'CursorColumn', 'StatusLine'],
+  \ 'bg+':     ['bg', 'CursorLine', 'CursorColumn'],
+  \ 'hl+':     ['fg', 'Statement'],
+  \ 'info':    ['fg', 'PreProc'],
+  \ 'border':  ['bg', 'StatusLine'],
+  \ 'prompt':  ['fg', 'DiagnosticHint'],
+  \ 'pointer': ['fg', 'Exception'],
+  \ 'marker':  ['fg', 'Keyword'],
+  \ 'spinner': ['fg', 'Label'],
+  \ 'header':  ['fg', 'Comment'] }
+]]
+)
