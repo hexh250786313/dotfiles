2dc4d387b97ba638341cc92523bbbb29a757d428
hexh <250786313@qq.com>
2022-11-28 19:53:21 +0800

    wip

--- .vim/rc.lua
+++ .vim/rc.lua
@@ -1,9 +1,13 @@
 vim.cmd(
   [[
 if match(&runtimepath, 'Defx') != -1
+  call defx#custom#column('filename', {
+    \   'min_width': 38,
+    \ })
   call defx#custom#option('_', {
     \   'show_ignored_files': 0,
-    \   'ignored_files': '.git',
+    \   'winwidth': 40,
+    \   'ignored_files': '.git,.vim',
     \ })
 endif
 ]]
