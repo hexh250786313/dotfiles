f39fae84e48284157f42126b5df4c651c992ab4a
hexh <250786313@qq.com>
2022-11-29 16:24:46 +0800

    wip

--- .vim/rc.lua
+++ .vim/rc.lua
@@ -1,12 +1,15 @@
 vim.cmd(
   [[
+let g:defx_win_width = 64
+" let g:defx_win_width = 40
+
 if match(&runtimepath, 'Defx') != -1
   call defx#custom#column('filename', {
-    \   'min_width': 38,
+    \   'min_width': g:defx_win_width - 2,
     \ })
   call defx#custom#option('_', {
     \   'show_ignored_files': 0,
-    \   'winwidth': 40,
+    \   'winwidth': g:defx_win_width,
     \   'ignored_files': '.git,.vim',
     \ })
 endif
