1e8a3c1d853e5ff04dbcd6b55c4dcd8d45bf9764
hexh <250786313@qq.com>
2022-12-05 16:22:58 +0800

    wip

--- .config/nvim/lua/user/plugin.lua
+++ .config/nvim/lua/user/plugin.lua
@@ -200,6 +200,7 @@ return packer.startup(
       {
         "sindrets/diffview.nvim",
         cmd = {"DiffviewFileHistory", "DiffviewOpen"},
+        commit = "6e514ce24afb1b7d104f0d736c22d5f2c1363037",
         keys = {{"n", "<space>gt"}, {"n", "<space>gg"}, {"n", "<space>gT"}},
         config = [[require("plugin.diffview")]]
       }
