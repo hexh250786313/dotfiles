499a09efd9e91da34af431e14fd58094b34602b9
hexh <250786313@qq.com>
2022-12-16 16:46:36 +0800

    wip

--- .config/my-config/sh/feh.sh
+++ .config/my-config/sh/feh.sh
@@ -3,7 +3,11 @@
 # feh --bg-fill ~/桌面/share/图片/wp7312731-blur-abstract-colors-hd-wallpapers.jpg ~/桌面/share/图片/88875544_p3.jpg ~/桌面/share/图片/88875544_p0.jpg
 
 while true; do
-  feh --bg-fill --no-fehbg --randomize ~/桌面/share/图片/wallpapers/random/* --bg-fill --no-fehbg --randomize ~/桌面/share/图片/wallpapers/random/*
-  sleep 300
+  if [[ ! -d ~/图片/WLKQBingWallpapers ]]; then
+    feh --bg-fill --no-fehbg --randomize ~/桌面/share/图片/wallpapers/random/* --bg-fill --no-fehbg --randomize ~/桌面/share/图片/wallpapers/random/*
+  else
+    feh --bg-fill --no-fehbg --randomize ~/图片/WLKQBingWallpapers/* --bg-fill --no-fehbg --randomize ~/图片/WLKQBingWallpapers/*
+  fi
+sleep 300
 done
 
