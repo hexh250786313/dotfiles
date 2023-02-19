b898a48349dfd6f5ebc96cd41c4613e9642e5f44
hexh <250786313@qq.com>
2023-02-20 07:57:10 +0800

    wip

--- .config/nvim/lua/plugin/defx.lua
+++ .config/nvim/lua/plugin/defx.lua
@@ -365,9 +365,9 @@ function! s:trim_right(str, trim)
   return substitute(a:str, printf('%s$', a:trim), '', 'g')
 endfunction
 
-nmap <silent> <Space>fo :Defx `getcwd()` -search-recursive=`expand('%:p')` -wincol=`&columns/9` -preview-width=`&columns/2` -winrow=`&lines/9` -winheight=`&lines/2` -preview_height=`&lines/1`<CR>
-" nmap <silent> <Space>fo :Defx -no-resume `getcwd()` -search-recursive=`expand('%:p')` -wincol=`&columns/9` -preview-width=`&columns/2` -winrow=`&lines/9` -winheight=`&lines/2` -preview_height=`&lines/1`<CR>
-nmap <silent> <Space>fr :Defx -no-resume `getcwd()` -search-recursive=`expand('%:p')` -wincol=`&columns/9` -preview-width=`&columns/2` -winrow=`&lines/9` -winheight=`&lines/2` -preview_height=`&lines/1`<CR>
+" nmap <silent> <Space>fr :Defx `getcwd()` -search-recursive=`expand('%:p')` -wincol=`&columns/9` -preview-width=`&columns/2` -winrow=`&lines/9` -winheight=`&lines/2` -preview_height=`&lines/1`<CR>
+nmap <silent> <Space>fo :Defx -no-resume `getcwd()` -search-recursive=`expand('%:p')` -wincol=`&columns/9` -preview-width=`&columns/2` -winrow=`&lines/9` -winheight=`&lines/2` -preview_height=`&lines/1`<CR>
+" nmap <silent> <Space>fr :Defx -no-resume `getcwd()` -search-recursive=`expand('%:p')` -wincol=`&columns/9` -preview-width=`&columns/2` -winrow=`&lines/9` -winheight=`&lines/2` -preview_height=`&lines/1`<CR>
 nmap <silent> <Space>fc :Defx -close<CR>
 
 ]]
