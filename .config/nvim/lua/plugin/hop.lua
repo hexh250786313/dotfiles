b8e28b007a70f16335f5d0dbd6d75d6d095f5b6f
hexh <250786313@qq.com>
2023-03-12 18:06:56 +0800

    wip

--- .config/nvim/lua/plugin/hop.lua
+++ .config/nvim/lua/plugin/hop.lua
@@ -4,6 +4,6 @@ vim.cmd([[
 " nmap f <cmd>:HopChar1<CR>
 " vmap f <cmd>:HopChar1<CR>
 nmap f <cmd>:HopChar2CurrentLine<CR>
-vmap f <cmd>:HopChar1CurrentLine<CR>
+vmap f <cmd>:HopChar2CurrentLine<CR>
 ]])
 
