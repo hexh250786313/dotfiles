4489be0f55c286e15309550c2df764524f0e4484
hexh <250786313@qq.com>
2023-03-20 21:51:09 +0800

    wip

--- .config/nvim/lua/hook/postinstall/all.sh
+++ .config/nvim/lua/hook/postinstall/all.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-cp /home/hexh/workspace/dotfiles/.config/nvim/lua/hook/postinstall/rplugin.vim /home/hexh/.local/share/nvim/
+cp ~/.config/nvim/lua/hook/postinstall/rplugin.vim /home/hexh/.local/share/nvim/
 
-sh -c /home/hexh/workspace/dotfiles/.config/nvim/lua/hook/postinstall/defx.sh
-sh -c /home/hexh/workspace/dotfiles/.config/nvim/lua/hook/postinstall/coc.sh
-sh -c /home/hexh/workspace/dotfiles/.config/nvim/lua/hook/postinstall/nvim-bqf.sh
+sh -c ~/.config/nvim/lua/hook/postinstall/defx.sh
+sh -c ~/.config/nvim/lua/hook/postinstall/coc.sh
+sh -c ~/.config/nvim/lua/hook/postinstall/nvim-bqf.sh
