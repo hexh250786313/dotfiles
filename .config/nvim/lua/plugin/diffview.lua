b8d7e4effbef617bfef761bbaf9a1338440952a6
hexh <250786313@qq.com>
2023-03-20 09:25:50 +0800

    wip

--- .config/nvim/lua/plugin/diffview.lua
+++ .config/nvim/lua/plugin/diffview.lua
@@ -21,7 +21,7 @@ require "diffview".setup {
       -- Change local options in diff buffers
       -- vim.cmd("call CocPrint(" .. bufnr .. ")")
       vim.cmd(":IndentBlanklineDisable")
-      vim.cmd(":set nocursorline")
+      vim.cmd(":setlocal nocursorline")
       vim.opt_local.wrap = false
       -- vim.opt_local.list = false
       -- vim.opt_local.colorcolumn = {80}
