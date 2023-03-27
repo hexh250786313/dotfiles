f5b77e04500b93a634e8ccd5347c852a86279faa
hexh <250786313@qq.com>
2023-03-27 23:40:48 +0800

    wip

--- .config/nvim/lua/hooks/postinstall/nvim-bqf.sh
+++ .config/nvim/lua/hooks/postinstall/nvim-bqf.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-sd '.*qlist:changedtick.*then\n.*return.*\n.*end' '' /home/hexh/.local/share/nvim/site/pack/packer/start/nvim-bqf/lua/bqf/main.lua
+sd '.*qlist:changedtick.*then\n.*return.*\n.*end' '' ~/.local/share/nvim/site/pack/packer/start/nvim-bqf/lua/bqf/main.lua
