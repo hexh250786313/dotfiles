ddcf662e06a9ea2578f1ab4ea3a2c3f8e0b64b4b
edwardhe <edward.he@songmao.tech>
2023-04-26 10:54:01 +0800

    wip

--- .config/nvim/lua/modules/base/plugins/statuscol.lua
+++ .config/nvim/lua/modules/base/plugins/statuscol.lua
@@ -3,8 +3,7 @@ vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 require("statuscol").setup({
   relculright = true,
   segments = {
-    {text = {builtin.foldfunc, " "}, click = "v:lua.ScFa"},
-    {text = {"%s"}, click = "v:lua.ScSa"},
+    {text = {builtin.foldfunc, " "}, click = "v:lua.ScFa"}, {text = {"%s"}, click = "v:lua.ScSa"},
     {text = {builtin.lnumfunc}, click = "v:lua.ScLa"}, {text = {" "}}
   }
 })
