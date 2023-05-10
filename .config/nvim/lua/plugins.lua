e928150770dba39f64f335892b9018ab2a9ab571
hexh <250786313@qq.com>
2023-05-11 01:37:39 +0800

    wip

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -83,6 +83,19 @@ return packer.startup(function(use)
     keys = {{"n", "<leader>qB"}}
   })
   use("~/.config/nvim/_self/plugins/avoid-scrolling-when-switch-buffers")
+  use({
+    "axkirillov/hbac.nvim",
+    config = function()
+      require("hbac").setup({
+        -- autoclose = true, -- set autoclose to false if you want to close manually
+        threshold = 3 -- hbac will start closing unedited buffers once that number is reached
+        -- close_command = function(bufnr)
+        --   vim.api.nvim_buf_delete(bufnr, {})
+        -- end,
+        -- close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
+      })
+    end
+  })
 
   -- ┌───────────────────────────────────────
   -- │  Code Module
