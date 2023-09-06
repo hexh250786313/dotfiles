6f1ffbe0b9e4b9dd04090db553e071b00b2ff340
hexh <edward.he@songmao.tech>
2023-09-06 18:40:23 +0800

    wip

--- .config/nvim/lua/modules/code/plugins/copilot.lua
+++ .config/nvim/lua/modules/code/plugins/copilot.lua
@@ -1,5 +1,5 @@
 local host = os.getenv("MY_HOST")
-vim.cmd("let g:copilot_proxy ='http://" .. host .. ":4780'")
+-- vim.cmd("let g:copilot_proxy ='http://" .. host .. ":4780'")
 vim.cmd([[
 imap <silent><script><expr> <c-I> copilot#Accept("\<CR>")
 let g:copilot_no_tab_map = v:true
