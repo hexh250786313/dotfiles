606a4c32b88bbd6c7fe6b622a722245ab2102047
hexh <edward.he@songmao.tech>
2023-09-12 10:46:37 +0800

    wip

--- .config/nvim/lua/modules/base/hooks/postinstall/coc.sh
+++ .config/nvim/lua/modules/base/hooks/postinstall/coc.sh
@@ -1,16 +1,25 @@
 #!/bin/zsh
 
+src_file="/home/hexh/.local/share/nvim/site/pack/packer/opt/coc.nvim/build/index.js"
+dest_file="/home/hexh/.local/share/nvim/site/pack/packer/opt/coc.nvim/build/index.js.bak"
+
+if [ ! -e "$dest_file" ]; then
+    cp "$src_file" "$dest_file"
+fi
+
 # for release
 # perl -0777 -i -pe 's/if\(t==\"\<plug\>\"\|\|this\.plugTs&&o-this\.plugTs<20\)\{this\.plugTs=o;return\}/if\(o-this\.plugTs\<50\)\{return;\}else\{this\.plugTs=o;\}/g' ./build/index.js
 # for master
 # perl -0777 -i -pe 's/.*if.*"\<plug\>\".*this\.plugTs.*20.*\n.*this\.plugTs.*now(.*\n){2}.*/if\(now-this\.plugTs\<50\)\{return;\}else\{this\.plugTs=now;\}/g' ./build/index.js
-sd '.*if.*"<plug>".*this\.plugTs.*20.*\n.*this\.plugTs.*now(.*\n){2}.*' 'if(now-this.plugTs<50){return;}else{this.plugTs=now;}' ~/.local/share/nvim/site/pack/packer/opt/coc.nvim/build/index.js
+# 这个的问题在于，导致无法输入中文，所以注释掉先
+# sd '.*if.*"<plug>".*this\.plugTs.*20.*\n.*this\.plugTs.*now(.*\n){2}.*' 'if(now-this.plugTs<50){return;}else{this.plugTs=now;}' ~/.local/share/nvim/site/pack/packer/opt/coc.nvim/build/index.js
 
 # for release
 # perl -0777 -i -pe 's/if\(o-this\.plugTs\<\d+\)\{return;\}else\{this\.plugTs=o;\}/if\(o-this\.plugTs\<20\)\{return;\}else\{this\.plugTs=o;\}/g' ./build/index.js
 # for master
 # perl -0777 -i -pe 's/if\(now-this\.plugTs\<\d+\)\{return;\}else\{this\.plugTs=now;\}/if\(now-this\.plugTs\<20\)\{return;\}else\{this\.plugTs=now;\}/g' ./build/index.js
-sd 'if\(now-this\.plugTs<\d+\)\{return;\}else\{this\.plugTs=now;\}' 'if(now-this.plugTs<20){return;}else{this.plugTs=now;}' ~/.local/share/nvim/site/pack/packer/opt/coc.nvim/build/index.js
+# 这个的问题在于，导致无法输入中文，所以注释掉先
+# sd 'if\(now-this\.plugTs<\d+\)\{return;\}else\{this\.plugTs=now;\}' 'if(now-this.plugTs<20){return;}else{this.plugTs=now;}' ~/.local/share/nvim/site/pack/packer/opt/coc.nvim/build/index.js
 
 # for both
 sd 'autocmd.*VimEnter.*call s:VimEnter\(\).*\n.*endif' 'autocmd VimEnter * call s:VimEnter()\n    else\n      call s:VimEnter()\n    endif' ~/.local/share/nvim/site/pack/packer/opt/coc.nvim/plugin/coc.vim
