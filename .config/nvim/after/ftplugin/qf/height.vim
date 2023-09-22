e47dcd0e37f1e70a6287811149f830b3137e0984
hexh <250786313@qq.com>
2023-09-22 21:14:27 +0800

    wip

---
+++ .config/nvim/after/ftplugin/qf/height.vim
@@ -0,0 +1,4 @@
+" 移到底部并且高度为6
+" wincmd J  | resize 6
+
+wincmd J
--- .config/nvim/lua/modules/base/hooks/postinstall/nvim-bqf.sh
+++ .config/nvim/lua/modules/base/hooks/postinstall/nvim-bqf.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
 sd '.*qlist:changedtick.*then\n.*return.*\n.*end' '' ~/.local/share/nvim/site/pack/packer/start/nvim-bqf/lua/bqf/main.lua
+sd 'api.nvim_win_get_height\(qwinid\)' '6' ~/.local/share/nvim/site/pack/packer/start/nvim-bqf/lua/bqf/layout.lua
+sd 'if incHeight == 0 then\n' 'if incHeight == 0 then api.nvim_win_set_height(qwinid, 6)\n' ~/.local/share/nvim/site/pack/packer/start/nvim-bqf/lua/bqf/layout.lua
