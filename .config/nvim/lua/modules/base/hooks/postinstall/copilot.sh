5c36e30cc37c539a0ab424476fa7402199658f53
hexh <250786313@qq.com>
2023-10-28 14:20:05 +0800

    wip

--- .config/nvim/lua/modules/base/hooks/postinstall/copilot.sh
+++ .config/nvim/lua/modules/base/hooks/postinstall/copilot.sh
@@ -1,11 +1,3 @@
 # sd '.*function!.*s:UpdatePreview.*abort.*\n.*try' 'function! s:UpdatePreview() abort\n  return\n  try' ~/.local/share/nvim/site/pack/packer/opt/copilot.vim/autoload/copilot.vim
 sd '=\["\\n"\]' '=["\\n\\n\\n"]' ~/.local/share/nvim/site/pack/packer/opt/copilot.vim/dist/agent.js
 sd 'stop=\[`\n`]' 'stop=[`\n\n\n`]' ~/.local/share/nvim/site/pack/packer/opt/copilot.vim/dist/agent.js
-
-if [ -f ~/Documents/nutstore/share/copilot/copilot.sh ]; then
-  ~/Documents/nutstore/share/copilot/copilot.sh
-fi
-
-if [ -f /mnt/c/Users/25078/Documents/share/copilot/copilot.sh ]; then
-  /mnt/c/Users/25078/Documents/share/copilot/copilot.sh
-fi
