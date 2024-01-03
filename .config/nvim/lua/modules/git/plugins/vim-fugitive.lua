323e92d4a7264fbdac26506052f8c1cfc88f0d14
hexh <250786313@qq.com>
2024-01-03 19:17:31 +0800

    WIP

--- .config/nvim/lua/modules/git/plugins/vim-fugitive.lua
+++ .config/nvim/lua/modules/git/plugins/vim-fugitive.lua
@@ -1,5 +1,5 @@
 vim.g.fugitive_pty = 0
 
 ---- 快捷键
-local wk = require("which-key")
-wk.register({ mode = { "n" }, ["<leader>gg"] = { "<cmd>Git<cr>", "Git status" } })
+-- local wk = require("which-key")
+-- wk.register({ mode = { "n" }, ["<leader>gg"] = { "<cmd>Git<cr>", "Git status" } })
