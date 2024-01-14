829576045a31e5ff9bdeb60d5adc5646637a8fac
hexh <250786313@qq.com>
2024-01-14 20:38:07 +0800

    WIP

--- .config/nvim/lua/modules/code/plugins/copilot.lua
+++ .config/nvim/lua/modules/code/plugins/copilot.lua
@@ -4,7 +4,7 @@ vim.cmd([[
 imap <silent><script><expr> <C-O> copilot#Accept("\<CR>")
 let g:copilot_no_maps = v:true
 let g:copilot_no_tab_map = v:true
-let g:copilot_idle_delay = 75
+let g:copilot_idle_delay = 50
 let g:copilot_filetypes = {
   \   'markdown': v:true,
   \ }
