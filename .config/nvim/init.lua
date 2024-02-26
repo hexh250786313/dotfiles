932e91ac1df35433fd6669e1aadcdff18e9cf337
hexh <250786313@qq.com>
2024-02-26 12:25:33 +0800

    WIP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,4 @@ require("plugins") -- 加载插件
 
 require("utils") -- 注册全局通用函数
 require("abbrs") -- 注册缩写
-
--- vim.cmd([[
--- set runtimepath^=/home/hexh/workspace/hexh/coc-pretty-ts-errors
--- ]])
+require("runtime") -- 运行时配置
