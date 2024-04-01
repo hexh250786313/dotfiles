7b33923f197bce61ab83d63014ff5a04b22bb910
hexh <250786313@qq.com>
2024-04-01 19:29:27 +0800

    WIP

--- .config/nvim/lua/modules/highlight/themes/__theme-config.lua
+++ .config/nvim/lua/modules/highlight/themes/__theme-config.lua
@@ -1,10 +1,15 @@
 -- theme-config.lua
 local M = {}
 
-local theme = require('modules/highlight/themes/rose-pine-neovim')
+local theme = require('modules/highlight/themes/catppuccin-nvim')
 
 function M.get_current_theme()
-  vim.cmd([[ set background=dark ]]) -- 背景 "light" | "dark"
+  vim.cmd([[
+  set background=dark " 背景 "light" | "dark"
+  " autocmd VimEnter * exec 'hi! @punctuation.bracket' .
+  "   \' guibg=' . _self#highlight#pick('@punctuation', 'bg') .
+  "   \' guifg=' . _self#highlight#pick('@punctuation', 'fg')
+  ]])
 
   -- 返回不同的主题配置对象
   return theme
