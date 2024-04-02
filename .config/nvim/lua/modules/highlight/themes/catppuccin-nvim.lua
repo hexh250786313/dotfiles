a7142a53f3643e4d15ea1c2296e9698b761a2869
hexh <250786313@qq.com>
2024-04-02 15:54:25 +0800

    WIP

--- .config/nvim/lua/modules/highlight/themes/catppuccin-nvim.lua
+++ .config/nvim/lua/modules/highlight/themes/catppuccin-nvim.lua
@@ -3,6 +3,9 @@ local M = {}
 M.name = "catppuccin/nvim"
 
 function M.setup()
+  require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+  })
   vim.cmd([[
   colorscheme catppuccin
   autocmd VimEnter * exec 'hi! @punctuation.bracket' .
