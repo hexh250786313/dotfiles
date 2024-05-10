269c65cd2a50f26e6d29896c06f2278d21cf4a1c
hexh <250786313@qq.com>
2024-05-10 15:05:54 +0800

    WIP

--- .config/nvim/lua/modules/code/plugins/copilot.lua
+++ .config/nvim/lua/modules/code/plugins/copilot.lua
@@ -1,8 +1,8 @@
 local host = os.getenv("MY_HOST")
 -- vim.cmd("let g:copilot_proxy ='http://" .. host .. ":4780'")
 vim.cmd([[
-" ---- 不需要 tab 的话就打开下面三行的注释
-" imap <silent><script><expr> <c-o> copilot#Accept("\<Tab>")
+imap <silent><script><expr> <c-o> copilot#Accept("\<Tab>")
+" ---- 不需要 tab 的话就打开下面两行的注释
 " let g:copilot_no_maps = v:true
 " let g:copilot_no_tab_map = v:true
 " -------------------
