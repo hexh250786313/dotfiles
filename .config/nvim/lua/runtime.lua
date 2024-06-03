2c684c829fa20c1f4e4f1245a1a4c8e32b59fc10
hexh <250786313@qq.com>
2024-06-03 10:23:42 +0800

    WIP

--- .config/nvim/lua/runtime.lua
+++ .config/nvim/lua/runtime.lua
@@ -2,6 +2,9 @@ vim.cmd([[
 set runtimepath^=~/workspace/hexh/coc-copilot
 ]])
 vim.cmd([[
+set runtimepath^=~/Desktop/test-coc/long-time-waiting
+]])
+vim.cmd([[
 set runtimepath^=~/workspace/hexh/test-coc/long-time-waiting
 ]])
 vim.cmd([[
