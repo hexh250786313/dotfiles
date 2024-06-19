068c08cf1da7b1e101f372db30bd12746ccd91c8
hexh <250786313@qq.com>
2024-06-19 10:04:52 +0800

    WIP

--- .config/nvim/lua/modules/base/hooks/postinstall/fzf-lua.sh
+++ .config/nvim/lua/modules/base/hooks/postinstall/fzf-lua.sh
@@ -4,4 +4,5 @@ src_file1="$HOME/.local/share/nvim/site/pack/packer/opt/fzf-lua/lua/fzf-lua/prev
 src_file2="$HOME/.local/share/nvim/site/pack/packer/opt/fzf-lua/lua/fzf-lua/actions.lua"
 
 sd '(if syntax_limit_reached > 0.*then[\s\S]*?end)' '' "$src_file1"
-sd 'and selected\[1\] ~= \"enter\"' 'and (type(actions.enter) == "function" or selected[1] ~= "enter")' "$src_file2"
+# enter 无效，已修复
+# sd 'and selected\[1\] ~= \"enter\"' 'and (type(actions.enter) == "function" or selected[1] ~= "enter")' "$src_file2"
