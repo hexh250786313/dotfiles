3f9e3220bbffa2c62d99c349e796f3f62e7de917
hexh <250786313@qq.com>
2024-08-28 11:36:15 +0800

    WIP

---
+++ .config/my-config/sh/forward-ports2.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+/mnt/c/Program\ Files/PowerShell/7/pwsh.exe -File "C:\Users\25078\Documents\Nutstore\share\startup\set_ports2.ps1"
+# /mnt/c/Program\ Files/PowerShell/7/pwsh.exe -File "C:\Users\25078\Documents\Nutstore\share\startup\unset_ports2.ps1"
