db17c2301dc5c0b3807ad43f30251ada29a7c411
hexh <250786313@qq.com>
2024-08-29 12:19:17 +0800

    WIP

--- .config/my-config/sh/keep-wsl-alive.sh
+++ .config/my-config/sh/keep-wsl-alive.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #-----------------------
-# keep wsl distro alive
+# keep wsl distro alive，既需要在 windows 定时计划中启动（ssh 的情况），也需要在 zshrc 中添加（wsl --shutdown 的情况）
+# 原本打算在 service 中加，但是不知道为什么一直无法启动，先不研究
 #-----------------------
 if ! pgrep -u "$(whoami)" -x "dbus-daemon" >/dev/null; then
   dbus-launch true
