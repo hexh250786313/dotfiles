adb7f91be8dec6a70032ee012410dd45c49bcd86
hexh <250786313@qq.com>
2024-10-10 10:35:36 +0800

    WIP

--- .config/nvim/lua/modules/base/hooks/postinstall/avante.sh
+++ .config/nvim/lua/modules/base/hooks/postinstall/avante.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
-target_file="$HOME/.local/share/nvim/site/pack/packer/opt/avante.nvim/lua/avante/sidebar.lua"
+sidebar="$HOME/.local/share/nvim/site/pack/packer/opt/avante.nvim/lua/avante/sidebar.lua"
+selection="$HOME/.local/share/nvim/site/pack/packer/opt/avante.nvim/lua/avante/selection.lua"
 
-perl -0777 -i -pe 's/(function Sidebar:close\(\)\n.*?self:delete_autocmds\(\)\n)(?!\s*require\("lualine"\)\.hide\(\{ unhide = true \}\))/\1  require("lualine").hide({ unhide = true })\n/s' $target_file
-perl -0777 -i -pe 's/(callback = function\(\)\n.*?self:focus\(\)\n)(?!\s*require\("lualine"\)\.hide\(\))/\1      require("lualine").hide()\n/s' $target_file
+
+perl -0777 -i -pe 's/(function Sidebar:close\(\)\n.*?self:delete_autocmds\(\)\n)(?!\s*require\("lualine"\)\.hide\(\{ unhide = true \}\))/\1  require("lualine").hide({ unhide = true })\n/s' $sidebar
+perl -0777 -i -pe 's/(callback = function\(\)\n.*?self:focus\(\)\n)(?!\s*require\("lualine"\)\.hide\(\))/\1      require("lualine").hide()\n/s' $sidebar
+sd 'function Selection:show_shortcuts_hints_popup\(\)\n(  if 1 then return nil end\n)?' 'function Selection:show_shortcuts_hints_popup()\n  if 1 then return nil end\n' $selection
