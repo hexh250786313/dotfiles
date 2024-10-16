0c866d57c223091c8d6705b37419810d9fce949c
hexh <250786313@qq.com>
2024-10-16 21:16:26 +0800

    WIP

--- .config/nvim/lua/modules/base/hooks/postinstall/avante.sh
+++ .config/nvim/lua/modules/base/hooks/postinstall/avante.sh
@@ -4,6 +4,6 @@ sidebar="$HOME/.local/share/nvim/site/pack/packer/opt/avante.nvim/lua/avante/sid
 selection="$HOME/.local/share/nvim/site/pack/packer/opt/avante.nvim/lua/avante/selection.lua"
 
 
-perl -0777 -i -pe 's/(function Sidebar:close\(\)\n.*?self:delete_autocmds\(\)\n)(?!\s*require\("lualine"\)\.hide\(\{ unhide = true \}\))/\1  require("lualine").hide({ unhide = true })\n/s' $sidebar
+perl -0777 -i -pe 's/(function Sidebar:close\(opts\)\n.*?self:delete_autocmds\(\)\n)(?!\s*require\("lualine"\)\.hide\(\{ unhide = true \}\))/\1  require("lualine").hide({ unhide = true })\n/s' $sidebar
 perl -0777 -i -pe 's/(callback = function\(\)\n.*?self:focus\(\)\n)(?!\s*require\("lualine"\)\.hide\(\))/\1      require("lualine").hide()\n/s' $sidebar
 sd 'function Selection:show_shortcuts_hints_popup\(\)\n(  if 1 then return nil end\n)?' 'function Selection:show_shortcuts_hints_popup()\n  if 1 then return nil end\n' $selection
