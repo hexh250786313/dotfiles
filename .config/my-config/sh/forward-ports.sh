fe18e211684064a472690be830e22464291ed7f7
hexh <250786313@qq.com>
2025-01-17 19:32:13 +0800

    WIP

--- .config/my-config/sh/forward-ports.sh
+++ .config/my-config/sh/forward-ports.sh
@@ -2,14 +2,14 @@
 
 # pwsh wsl-netsh-set-remote 192.168.10.68 3000
 
-for port in {7766..7776} 22 8877 8878; do
+for port in {7766..7776} 22 8877 8878 5555 5554; do
     echo "Processing port $port..."
     zsh -ic "/home/hexh/.config/my-config/sh/pwsh.sh fw-port-set $port"
     zsh -ic "/home/hexh/.config/my-config/sh/pwsh.sh wsl-netsh-unset-remote 192.168.10.160 $port"
     zsh -ic "/home/hexh/.config/my-config/sh/pwsh.sh wsl-netsh-set-remote 192.168.10.160 $port"
 done
 
-for port in {4781}; do
+for port in 4781 33272; do
     echo "Processing port $port..."
     zsh -ic "/home/hexh/.config/my-config/sh/pwsh.sh fw-port-set $port"
     zsh -ic "/home/hexh/.config/my-config/sh/pwsh.sh sudo pwsh -c \"netsh interface portproxy delete v4tov6 listenport=$port listenaddress=192.168.10.160\""
