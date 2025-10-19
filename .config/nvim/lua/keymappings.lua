ef574416ade2bfc2fcd9f4ae21e1fdb5398bb987
hexh <250786313@qq.com>
2025-10-19 11:03:23 +0800

    WIP

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -5,9 +5,10 @@ require("modules.git.keymappings")
 require("modules.motion.keymappings")
 require("modules.highlight.keymappings")
 
--- 取消默认 keymap gra/grn/gri/grr
+-- 取消默认 keymap gra/grn/gri/grr/grt
 vim.keymap.del("n", "gra")
 vim.keymap.del("n", "grn")
 vim.keymap.del("n", "gri")
 vim.keymap.del("n", "grr")
+vim.keymap.del("n", "grt")
 -- vim.keymap.del("n", "K")
